05f1a7d8123826d42ebaf78ddb3ac396de69b74a
Fady Megally <fmegally@gmail.com>
2020-11-01 11:28:09 -0500

    uart driver WIP

--- uart.c
+++ uart.c
@@ -6,46 +6,46 @@
  */ 
 #include "uart.h"
 
-void UART_Init(uint32_t baud, uint8_t parity, uint8_t byte_size )
+void UART_init(const uart* base, uint32_t baud, uint8_t parity, uint8_t byte_size )
 {
-	UBRR0  = (F_OSC/(16 * baud)) - 1;                                       //set baud rate countdown register. formula for UBRR register is in atmel data sheet
-	UCSR0B = (~(0x01 << UCSZ02) & UCSR0B) | (byte_size & 0x04);             //byte size settings are split over 2 registers so it has to be done in 2 steps
-	UCSR0C = (~(0x03 << UCSZ00) & UCSR0C) | ((byte_size & 0x03) << UCSZ00);
-	UCSR0C = (~(0x03 << UPM00) & UCSR0C) | (parity << UPM00);               //select parity
-	UCSR0B |= (1 << RXEN0) | (1 << TXEN0);                                  //enable serial TX and RX
-	UCSR0B |= (1 << RXCIE0);                                                //enable RX complete interrupt
+	device->UBRRn  = (F_OSC/(16 * baud)) - 1;                                       //set baud rate countdown register. formula for UBRR register is in atmel data sheet
+	device->UCSRnB = (~(0x01 << UCSZn2) & UCSRnB) | (byte_size & 0x04);             //byte size settings are split over 2 registers so it has to be done in 2 steps
+	device->UCSRnC = (~(0x03 << UCSZn0) & UCSRnC) | ((byte_size & 0x03) << UCSZn0);
+	device->UCSRnC = (~(0x03 << UPMn0) & UCSRnC) | (parity << UPMn0);               //select parity
+	device->UCSRnB |= (1 << RXENn) | (1 << TXENn);                                  //enable serial TX and RX
+	device->UCSRnB |= (1 << RXCIEn);                                                //enable RX complete interrupt
 	return;
 }
 
 static
-void UART_wait_TX(void)
+void UART_wait_TX(const uart* base)
 {
-	while(!(UCSR0A & (1 << UDRE0)));
+	while(!(base->UCSRnA & (1 << UDRE0)));
 	return;
 }
 
-void UART_putc(char c)
+void UART_putc(const uart* base, const char c)
 {
-    UART_wait_TX();
-    UDR0 = c;
+    UART_wait_TX(base);
+    base->UDRn = c;
     return;
 }
 
-void UART_write(const uint8_t* buff, uint8_t len)
+void UART_write(const uart* base, const uint8_t* buff, uint8_t len)
 {
 	uint8_t i;
 	for (i = 0; i < len; i++) {
-		UART_wait_TX();
-		UDR0 = *(buff + i);
+		UART_wait_TX(base);
+		UDRn = *(buff + i);
 	}
 	return;
 }
 
-void UART_print(const char* str){
+void UART_print(const uart* base, const char* str){
 	uint16_t i = 0;
 	while (*(str + i) != '\0'){
-		UART_wait_TX();
-		UDR0 = *(str + i++);
+		UART_wait_TX(base);
+		base->UDRn = *(str + i++);
 	}
 	return;
 }
--- uart.h
+++ uart.h
@@ -12,7 +12,59 @@
 
 #include <avr/io.h>
 #include "common.h"
- 
+
+struct uart
+{
+	uint8_t  UCSRnA;
+	uint8_t  UCSRnB;
+	uint8_t  UCSRnC;
+	uint8_t  _padding_0;
+	uint16_t UBRRn;
+	uint8_t  UDRn;
+	uint8_t  _padding_1;
+};
+
+typedef struct uart uart_t;
+
+#define UART0_BASE	 (0xC0)
+#define UART0 		 ((uart *) UART0_BASE)
+
+#define UART1_BASE	 (0xC8)
+#define UART1 		 ((uart *) UART0_BASE)
+
+#define UART2_BASE	 (0xD0)
+#define UART2 		 ((uart *) UART0_BASE)
+
+#define UART3_BASE	 (0x130)
+#define UART3 		 ((uart *) UART0_BASE)
+
+#define RXCn    7
+#define TXCn    6
+#define UDREn   5
+#define FEn     4
+#define DORn    3
+#define UPEn    2
+#define U2Xn    1
+#define MPCMn   0
+
+#define RXCIEn  7
+#define TXCIEn  6
+#define UDRIEn  5
+#define RXENn   4
+#define TXENn   3
+#define UCSZn2  2
+#define RXB8n   1
+#define TXB8n   0
+
+#define UMSELn1 7
+#define UMSELn0 6
+#define UPMn1   5
+#define UPMn0   4
+#define USBSn   3
+#define UCSZn1  2
+#define UCSZn0  1
+#define UCPOLn  0
+
 #define UART_PARITY_NONE 0
 #define UART_PARITY_EVEN 2
 #define UART_PARITY_ODD  3
@@ -26,23 +78,9 @@
 #define UART_CHAR_SIZE_8BIT 3
 #define UART_CHAR_SIZE_9BIT 7
 
-struct uart
-{
-	uint8_t  UCSRnA;
-	uint8_t  UCSRnB;
-	uint8_t  UCSRnC;
-	uint8_t  _padding_0;
-	uint16_t UBRRn;
-	uint8_t  UDRn;
-	uint8_t  _padding_1;
-};
 
-volatile struct uart* const UART_0;
-volatile struct uart* const UART_1;
-volatile struct uart* const UART_2;
-
-void UART_init(uint32_t baud, uint8_t parity, uint8_t byte_size );
-void UART_write(const uint8_t* buff, uint8_t len);
-void UART_print(const char* str);
-void UART_putc(char c);
+void UART_init(const uart* base, uint32_t baud, uint8_t parity, uint8_t byte_size );
+void UART_write(const uart* base, const uint8_t* buff, uint8_t len);
+void UART_print(const uart* base, const char* str);
+void UART_putc(const uart* base, char c);
 #endif /* UART_H_ */
