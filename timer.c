83502bd6b6a1e1062473f70b58310a8836e85400
Fady Megally <fmegally@gmail.com>
2020-11-08 12:33:38 -0500

    timer driver WIP

--- timer.c
+++ timer.c
@@ -1,3 +1,37 @@
 #include "timer.h"
 
-volatile struct tcnt * const tcnt_0 = (volatile struct tcnt *) &TCCR0A;
+void timer_init (tcnt_16b_t* timer, enum tcnt_modes mode)
+{
+	return;
+}
+
+void timer_start (tcnt_16b_t* timer, enum tcnt_clk_src)
+{
+	return;
+}
+
+void timer_stop (tcnt_16b_t* timer)
+{
+	return;
+}
+
+void timer_get_input_capture (tcnt_16b_t* timer, uint16_t *dst)
+{
+	return;
+}
+
+void timer_set_pwm_dc (tcnt_16b_t* timer, uint16_t duty_cycle)
+{
+	return;
+}
+
+void timer_set_pwm_freq (tcnt_16b_t* timer, uint16_t freq)
+{
+	return;
+}
+
+void timer_udelay (tcnt_16b_t * timer, uint16_t usec)
+{
+	return;
+}
+
