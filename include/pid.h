72351829590171c2c86a405a76093612c255826c
Fady Megally <fmegally@protonmail.com>
2021-01-14 08:50:39 -0500

    working on PID loop

--- include/pid.h
+++ include/pid.h
@@ -1,3 +1,17 @@
+#ifndef PID_H_
+#	define PID_H_
+
+#include <stdint.h>
+
+enum PID_parameter
+{
+	KP,
+	KI,
+	KD,
+	DEAD_BAND,
+	ANTI_WINDUP
+};
+
 struct PID
 {
 	uint8_t kp;
@@ -8,16 +22,19 @@ struct PID
 	int16_t err_;
 	int16_t err__;
 
-	int16_t err_integ;
-	int16_t err_diff;
+	int16_t i_err;
+	int16_t d_err;
 
-	uint8_t dead_band;
-	int16_t cv;
-	int16_t sp;
-	int16_t pv;
+	int8_t deadband;
+	int16_t contvar;
+	int16_t setpnt;
+	int16_t procvar;
 };
 
 
-	
+int16_t PID_step(struct PID *pid, int16_t cv);
+void PID_set_sp(struct PID *pid, int16_t sp);
+void PID_init(struct PID *pid);
+void PID_set_parameter(struct PID *pid, enum PID_parameter, int16_t val);
 
-
+#endif
